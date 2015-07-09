c8b7ffe085bce291974ee3496f91e6d9b70bdab1
Deutro <patrick.engelkes@gmail.com>
2015-07-09 17:41:01 +0200

    Finish first search form.

---
+++ app/controllers/flights_controller.rb
@@ -0,0 +1,7 @@
+class FlightsController < ApplicationController
+  def index
+    @flights = Flight.search(params)
+    @airport_codes = Airport.all.collect { |a| [a.code, a.id] }
+    @num_passengers = (1..4).to_a.collect { |x| [x, x] }
+  end
+end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,11 @@
 class Flight < ActiveRecord::Base
   belongs_to :from_airport, class_name: 'Airport'
   belongs_to :to_airport, class_name: 'Airport'
+
+  def self.search(params)
+    if params[:search]
+      Flight.where(from_airport_id: params[:from], to_airport_id: params[:to])
+            .includes(:from_airport, :to_airport)
+    end
+  end
 end
